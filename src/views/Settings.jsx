6c00fd00226ae517e8bcbcb85ef6c249769d94fd
Akira360 <akira360.v@gmail.com>
2020-02-25 23:27:35 +0300

    lesson23+

--- src/views/Settings.jsx
+++ src/views/Settings.jsx
@@ -1,12 +1,23 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+const SettingsItem = props => {
+  return (
+    <div className="SettingsItem">
+      <NavLink to={"/Settings/" + props.id}>{props.name}</NavLink>
+    </div>
+  )
+}
 
 const Settings = () => {
   return (
-    <ul className="Statistics">
-      Settings
+    <ul className="Settings">
+      <SettingsItem id='1' name='SettingsItem(1)' />
+      <SettingsItem id='2' name='SettingsItem(2)'  />
+      <SettingsItem id='3' name='SettingsItem(3)'  />
+      <SettingsItem id='4' name='SettingsItem(4)'  />
     </ul>
   )
 }
 
 export default Settings
-
