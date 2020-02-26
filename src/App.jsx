b2e49c7d820fac6cd22216258d5f1fb522545e16
Akira360 <akira360.v@gmail.com>
2020-02-26 12:57:03 +0300

    rt01

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 // components app
 import Navbar from './components/app/Navbar'
 import Sidebar from './components/app/Sidebar'
+import Dialogs from './components/Dialogs/Dialogs.jsx'
 // components app //
 // views
 import Employees from './views/Employees'
@@ -10,14 +11,14 @@ import Statistics from './views/Statistics'
 import Settings from './views/Settings'
 // views //
 // Route
-import { BrowserRouter, Route  } from 'react-router-dom'
+import { BrowserRouter, Route } from 'react-router-dom'
 // Route //
 // css
 import './assets/sass/index.sass'
 import './App.css'
 // css //
 
-const App = () => {
+const App = (props) => {
   return (
     <BrowserRouter>
       <div className="app-wrapper">
@@ -25,10 +26,11 @@ const App = () => {
         <div className="app-wrappers">
           <Sidebar />
           <div className="app-wrapper-content">
-            <Route path='/Employees' component={Employees} />
-            <Route path='/Equipment' component={Equipment} />
-            <Route path='/Statistics' component={Statistics} />
-            <Route path='/Settings' component={Settings} />
+            <Route path="/Employees" render={() => <Employees />} />
+            <Route path="/Equipment" render={() => <Equipment />} />
+            <Route path="/Statistics" render={() => <Statistics />} />
+            <Route path="/Settings" render={() => <Settings />} />
+            <Route path="/dialogs" render={() => <Dialogs state={props.state.dialogsPage} />} />
           </div>
         </div>
       </div>
