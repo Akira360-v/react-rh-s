64feba340598f89f20c66cf8df0c4fffadfafa4f
Akira360 <akira360.v@gmail.com>
2020-03-04 13:47:40 +0300

    rename state => store

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // import * as serviceWorker from './serviceWorker'
 import React from 'react'
 import ReactDOM from 'react-dom'
-import store from './redux/state'
+import store from './redux/store'
 import './index.css'
 import App from './App'
 import { BrowserRouter } from 'react-router-dom'
