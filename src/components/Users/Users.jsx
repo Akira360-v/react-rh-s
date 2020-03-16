e738e154e42d6b679d4c825e450f7d3f9036a8fe
v-ignatenko <49681882+v-ignatenko@users.noreply.github.com>
2020-03-16 12:22:35 +0300

    rt20 (l)53+

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -3,69 +3,93 @@ import React from 'react'
 import * as axios from 'axios'
 import LogoUzerDefault from '../../assets/images/LogoUzerDefault.png'
 
-let Users = props => {
-  if (props.users.length === 0) {
+class Users extends React.Component {
+  constructor(props) {
+    super(props)
+    // if (this.props.users.length === 0) {
     // axios.get("http://base.rh-s.com/api/employees").then(response => {
     axios
       .get('https://social-network.samuraijs.com/api/1.0/users')
       // .get('http://base.rh-s.com/api/employees')
       .then(response => {
         // debugger
-        props.setUsers(response.data.items)
+        this.props.setUsers(response.data.items)
       })
     // .catch(error => {
     //   debugger
     //   console.error(error)
     // })
+    // }
   }
 
-  return (
-    <div className="Users">
-      {props.users.map(u => (
-        <div className="Users__list" key={u.id}>
-          <span>
-            <div>
-              {/* <img src="" alt="logo" className="list_img" /> */}
-              <img
-                src={u.photos.small != null ? u.photos.small : LogoUzerDefault}
-                alt="logo"
-                className="list_img"
-              />
-            </div>
-            <div>
-              {u.followed ? (
-                <button
-                  onClick={() => {
-                    props.unfollow(u.id)
-                  }}
-                >
-                  Unfollow
-                </button>
-              ) : (
-                <button
-                  onClick={() => {
-                    props.follow(u.id)
-                  }}
-                >
-                  Follow
-                </button>
-              )}
-            </div>
-          </span>
-          <span>
+  // getUsers = () => {
+  //   if (this.props.users.length === 0) {
+  //     // axios.get("http://base.rh-s.com/api/employees").then(response => {
+  //     axios
+  //       .get('https://social-network.samuraijs.com/api/1.0/users')
+  //       // .get('http://base.rh-s.com/api/employees')
+  //       .then(response => {
+  //         // debugger
+  //         this.props.setUsers(response.data.items)
+  //       })
+  //     // .catch(error => {
+  //     //   debugger
+  //     //   console.error(error)
+  //     // })
+  //   }
+  // }
+  render() {
+    return (
+      <div className="Users">
+        {/* <button onClick={this.getUsers}>get users</button> */}
+        {this.props.users.map(u => (
+          <div className="Users__list" key={u.id}>
             <span>
-              <div>{u.name}</div>
-              <div>{u.status}</div>
+              <div>
+                {/* <img src="" alt="logo" className="list_img" /> */}
+                <img
+                  src={
+                    u.photos.small != null ? u.photos.small : LogoUzerDefault
+                  }
+                  alt="logo"
+                  className="list_img"
+                />
+              </div>
+              <div>
+                {u.followed ? (
+                  <button
+                    onClick={() => {
+                      this.props.unfollow(u.id)
+                    }}
+                  >
+                    Unfollow
+                  </button>
+                ) : (
+                  <button
+                    onClick={() => {
+                      this.props.follow(u.id)
+                    }}
+                  >
+                    Follow
+                  </button>
+                )}
+              </div>
             </span>
             <span>
-              {/* <div>{u.location.country}</div> */}
-              {/* <div>{u.location.city}</div> */}
+              <span>
+                <div>{u.name}</div>
+                <div>{u.status}</div>
+              </span>
+              <span>
+                {/* <div>{u.location.country}</div> */}
+                {/* <div>{u.location.city}</div> */}
+              </span>
             </span>
-          </span>
-        </div>
-      ))}
-    </div>
-  )
+          </div>
+        ))}
+      </div>
+    )
+  }
 }
 
 export default Users
